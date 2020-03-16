fc937786f133ba741390df577931c3c677b16516
ytwwww <52704719+ytwwww@users.noreply.github.com>
2020-03-15 22:02:13 -0300

    Add files via upload

---
+++ Assignment3/ext2_mkdir.c
@@ -0,0 +1,30 @@
+/* from handout:
+ext2_mkdir:
+This program takes two command line arguments.
+The first is the name of an ext2 formatted virtual disk.
+The second is an absolute path on your ext2 formatted disk.
+
+The program should work like mkdir,
+creating the final directory on the specified path on the disk.
+
+If any component on the path to the location
+where the final directory is to be created does not exist
+or if the specified directory already exists,
+then your program should return the appropriate error (ENOENT or EEXIST).
+
+Again, please read the specifications to make sure
+you're implementing everything correctly
+(e.g., directory entries should be aligned to 4B,
+entry names are not null-terminated, etc.).
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    char *err_message = "USAGE: ./ext2_mkdir disk_name path_to_disk\n";
+    if (argc != 3) {
+        fprintf(stderr, "%s", err_message);
+        exit(1);
+    }
+    return 0;
+}
