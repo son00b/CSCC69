939c732a56ec2856c8d6958c6588e1941312532e
silverxpurple <silverxpurple@penguin>
2020-03-22 13:22:08 -0400

    fixed compile warnings

---
+++ Assignment3/ext2_rm_bonus.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    // char *err_message = "USAGE: ./ext2_mkdir disk_name path_to_disk\n";
+    // if (argc != 3) {
+    //     fprintf(stderr, "%s", err_message);
+    //     exit(1);
+    // }
+    // // name of disk
+    // char *name = argv[1];
+    // // path of disk
+    // char *path = argv[2];
+    // // the disk
+    // unsigned char *disk = saveImage(name);
+
+    // get the parent inode
+
+    // check if dir exists
+
+    // if not, mkdir
+
+
+    return 0;
+}
