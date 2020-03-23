94b98591bdc533fc15bde756eb41a70435c9da1e
silverxpurple <silverxpurple@penguin>
2020-03-23 16:30:44 -0400

    saving

--- Assignment3/ext2_ls.c
+++ Assignment3/ext2_ls.c
@@ -26,6 +26,8 @@ and refrain from printing the . and ..
 #include <stdio.h>
 #include <stdlib.h>
 #include <libgen.h>
+#include <string.h>
+
 #include "ext2.h"
 #include "helper.c"
 
@@ -44,42 +46,160 @@ int main(int argc, char *argv[]) {
     unsigned char *disk = saveImage(disk_name);
     fprintf(stderr, "%s", disk);
 
+        struct ext2_super_block *sb = (struct ext2_super_block *)(disk + 1024);
+
+
+    /**********************************************************************************/
+    /************************************** TASK 1 ************************************/
+    /**********************************************************************************/
+
+
     // Index to the group descriptor, cast to the required struct
-    struct ext2_group_desc *bgd = (struct ext2_group_desc *) (disk + 2048);
+    struct   ext2_group_desc *bgd = (struct ext2_group_desc *) (disk + 2048);
 
-    fprintf(stderr, "%s", "disk");
-    // Get the attributes needed
-    unsigned int inode_table = bgd->bg_inode_table;
-    // get inode
-    struct ext2_inode* in = (struct ext2_inode*) (disk + inode_table * EXT2_BLOCK_SIZE);
-    // get the attributes needed
-    unsigned short mode = in->i_mode; // file mode
-    fprintf(stderr, "%d", mode);
-    unsigned int *block = in->i_block; //Pointers to blocks
 
-    // if given path is file or link, print just the file   
-    if (mode & EXT2_S_IFREG || mode & EXT2_S_IFLNK ) {
-        // check if file/link exists, if so print
-        printf("%s", basename(disk_path));
-    } 
-    // if given path is a directory, print everything in the directory
-    else if (mode & EXT2_S_IFDIR) {
-        fprintf(stderr, "%s", "asd");
-        unsigned long pos = (unsigned long) disk + (int) *block * EXT2_BLOCK_SIZE;
-        struct ext2_dir_entry_2 *dir = (struct ext2_dir_entry_2 *) pos;
-        do {
-            // Get the length of the current block and type
-            int cur_len = dir->rec_len;
-            // Print the current directory entry
-            printf("name: %s\n", dir->name);
-            // Update position and index into it
-            pos = pos + cur_len;
-            dir = (struct ext2_dir_entry_2 *) pos;
+    
+    /**********************************************************************************/
+    /************************************** TASK 2 ************************************/
+    /**********************************************************************************/
 
-            // Last directory entry leads to the end of block. Check if 
-            // Position is multiple of block size, means we have reached the end
-        } while (pos % EXT2_BLOCK_SIZE != 0);
+
+    
+    /******************************* Block Bitmap *************************************/
+
+    // Get block bitmap
+    char *bm = (char *) (disk + (bgd->bg_block_bitmap * EXT2_BLOCK_SIZE));
+    // counter for shift
+    printf("block bitmap: ");
+    int index = 0;
+    for (int i = 0; i < sb->s_blocks_count; i++) {
+        unsigned c = bm[i / 8];                     // get the corresponding byte
+        printf("%d", (c & (1 << index)) > 0);       // Print the correcponding bit
+        if (++index == 8) (index = 0, printf(" ")); // increment shift index, if > 8 reset.
     }
+    printf("\n");
+
+    /******************************* Inode Bitmap *************************************/
+    /************************* + store used inodes ************************************/
+
+    // Get inode bitmap
+    char *bmi = (char *) (disk + (bgd->bg_inode_bitmap * EXT2_BLOCK_SIZE));
+    // Want to keep track of the used inodes in this array
+    int inum[32];
+    inum[0] = 1;    // Root inode is Inode number 2, index 1
+    // current size of the array
+    int inumc = 1;  // because we stored the first one
+    // counter for shift
+    printf("inode bitmap: ");
+    int index2 = 0;
+    for (int i = 0; i < sb->s_inodes_count; i++) {
+        unsigned c = bmi[i / 8];                     // get the corresponding byte
+        printf("%d", (c & (1 << index2)) > 0);       // Print the correcponding bit
+        // If that bit was a 1, inode is used, store it into the array.
+        // Note, this is the index number, NOT the inode number
+        // inode number = index number + 1
+        if ((c & (1 << index2)) > 0 && i > 10) {    // > 10 because first 11 not used
+            inum[inumc++] = i;
+        }
+        if (++index2 == 8) (index2 = 0, printf(" ")); // increment shift index, if > 8 reset.
+    }
+    printf("\n\n");
+
+    struct ext2_inode* in = (struct ext2_inode*) (disk + bgd->bg_inode_table * EXT2_BLOCK_SIZE);
+
+
+
+    /******************************* Print Inodes *************************************/
+
+
+    /**** The following array is used to keep track of directories ****/
+    int dirs[128];
+    int dirsin = 0;
+
+    printf("Inodes:\n");
+
+    // Go through all the used inodes stored in the array above
+    for (int i = 0; i < inumc; i++) {
+        // Remember array stores the index
+        struct ext2_inode* curr = in + inum[i];
+        int inodenum = inum[i] + 1;     // Number = index + 1
+        char type = (S_ISDIR(curr->i_mode)) ? 'd' : ((S_ISREG(curr->i_mode)) ? 'f' : 's');
+        printf("[%d] type: %c size: %d links: %d blocks: %d\n", inodenum, type, \
+            curr->i_size, curr->i_links_count, curr->i_blocks);     // Print Inode info
+        
+        // Now to print all the blocks for the inode
+        printf("[%d] Blocks: ", inodenum);  
+        // Get the array of blocks from inode
+        unsigned int *arr = curr->i_block;
+        // Loop through and print all value till a 0 is seen in the array
+        while(1) {
+            if (*arr == 0) {
+                break;
+            }
+            // If it's a directory, add to the array.
+            if (type == 'd') {
+                dirs[dirsin++] = *arr;
+            }
+            printf("%d ", *arr++);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    /**********************************************************************************/
+    /************************************** TASK 3 ************************************/
+    /**********************************************************************************/
+
+    char *string = "Hello! /We are learning about strtok";
+    char *cur;
+
+    string = strdup(disk_path);
+    printf("Original string: '%s'\n",string);
+    // skip root directory
+    cur = strsep(&string,"/");
+    char *filename = basename(disk_path);
+    while( (cur = strsep(&string,"/")) != NULL ){
+        printf("Directory Blocks:\n");
+        // For all the directory blocks
+        for (int i = 0; i < dirsin; i++) {
+            // Get the block number
+            int blocknum = dirs[i];
+            // Get the position in bytes and index to block
+            unsigned long pos = (unsigned long) disk + blocknum * EXT2_BLOCK_SIZE;
+            struct ext2_dir_entry_2 *dir = (struct ext2_dir_entry_2 *) pos;
+
+            printf("    DIR BLOCK NUM: %d (for inode %d)\n", blocknum, dir->inode);
+
+            do {
+                char *name = dir->name;
+                // Get the length of the current block and type
+                int cur_len = dir->rec_len;
+                if (strcmp(name, cur)){
+                    printf("name: %s %s\n", cur, filename);
+                    printf("%lu %lu", strlen(cur), strlen(filename));
+                    printf("name: %d\n", strncmp(cur, filename, strlen(cur)));
+                    if (strcmp(cur, filename)){
+                        // Print the current directory entry
+                        printf("name: %.*s\n", dir->name_len, dir->name);
+                    }
+                }
+                
+                
+                // Update position and index into it
+                pos = pos + cur_len;
+                dir = (struct ext2_dir_entry_2 *) pos;
+
+                // Last directory entry leads to the end of block. Check if 
+                // Position is multiple of block size, means we have reached the end
+            } while (pos % EXT2_BLOCK_SIZE != 0);
+        }
+
+    }
+        
+
+
+   
+    
     
     return 0;
 }
