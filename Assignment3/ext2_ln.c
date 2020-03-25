c1e9888203534e1b037065269476a68285ced427
ytwwww <yacat24@gmail.com>
2020-03-25 11:06:40 -0400

    handle -s

--- Assignment3/ext2_ln.c
+++ Assignment3/ext2_ln.c
@@ -23,12 +23,37 @@ use the ext2 specs and ask on the discussion board.
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     char *err_message = "USAGE: ./ext2_ln disk_name path_to_disk1 path_to_disk2 [-s]\n";
     if (argc != 4 && argc != 5) {
         fprintf(stderr, "%s", err_message);
         exit(1);
+    }
+
+    int sflag = 0; // 1 when "-s" is provided; 0 when "-s" not provided.
+    if (argc == 5) {
+        int i;
+        for (i = 1; i < argc; i++) {
+            if (strncmp(argv[i], "-s", strlen("-s")) == 0) {
+                if (i == 1) {
+                    fprintf(stderr, "-s should be after the disk image argument \n");
+                    exit(1);
+                } else if (i == 2) {
+                    pathindexA = 3;
+                    pathindexB = 4;
+                } else if (i == 3) {
+                    pathindexB = 4;
+                }
+                sflag = 1;
+            }
+        }
+        // if there are 4 command line argument, "-s" should be provided.
+        if (sflag == 0) {
+            fprintf(stderr, "-s should be provided.");
+            exit(1);
+        }
     }
     return 0;
 }
